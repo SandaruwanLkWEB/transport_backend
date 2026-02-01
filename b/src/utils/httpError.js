38003c2248f94a9f01f5630ff93f419d3e2efe73
MR_Sandaruwan <sandaruwanjayalath95@gmail.com>
2026-02-01 18:19:48 +0530

    Delete b directory

--- b/src/utils/httpError.js
+++
@@ -1,7 +0,0 @@
-function httpError(status, message, expose = true) {
-  const err = new Error(message);
-  err.status = status;
-  err.expose = expose;
-  return err;
-}
-module.exports = { httpError };
